e5847963422a4f81510e71e9017aa7848977d760
Player003 <matias_roldan89@hotmail.com>
2022-10-22 05:26:35 -0300

    cambio brandsModels

--- Entities/DataTransferObjects/BrandsModels- Dto/BrandsModelsForSelectDto.cs
+++ Entities/DataTransferObjects/BrandsModels- Dto/BrandsModelsForSelectDto.cs
@@ -1,17 +1,14 @@
-﻿using Entities.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Entities.DataTransferObjects.BrandsModels__Dto
 {
     public class BrandsModelsForSelectDto
     {
-        public int FK_TypeVehicleID { get; set; }
+        public int TypeID { get; set; }
 
         public string Type { get; set; }
 
-
-
-         public ICollection<BrandsModelsDto> BrandModels { get; set; }
+        public BrandsSelect Brands { get; set; }
     }
 
     public class TypesSelect
@@ -21,13 +18,10 @@ namespace Entities.DataTransferObjects.BrandsModels__Dto
         public string Type { get; set; }
     }
 
-    public class BrandsModelsDto
+    public class BrandsSelect
     {
-        public int FK_BrandID { get; set; }
+        public int BrandID { get; set; }
         public string BrandsName { get; set; }
-
-        public int FK_ModelID { get; set; }
-        public string ModelName { get; set; }
     }
 
     public class ModelsSelect
